6a03d6e44e3e4ab7e18dd64aa099f69696561f59
s110b <111761629+s110b@users.noreply.github.com>
2023-11-24 11:45:04 +0800

    c
    c

--- compress-images.sh
+++ compress-images.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 图片原始目录和压缩后目录
+
 ORIGIN_DIR="/images/origin"
 COMPRESSED_DIR="/images/compressed"
 
-# 创建压缩后的目录
+
 mkdir -p "$COMPRESSED_DIR"
 
 ls -al $ORIGIN_DIR
 
-# 压缩图片
+
 find "$ORIGIN_DIR" -type f \( -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' \) -print0 | xargs -0 -P 4 -I {} squoosh-cli --webp '{}' --output-dir "$COMPRESSED_DIR"
