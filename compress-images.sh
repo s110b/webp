49d1dc80f15f9fb9f9811c7cf2b111509ae1f559
s110b <111761629+s110b@users.noreply.github.com>
2023-11-24 13:41:32 +0800

    c
    c

--- compress-images.sh
+++ compress-images.sh
@@ -7,33 +7,12 @@ COMPRESSED_DIR="/images/compressed"
 
 mkdir -p /images/compressed
 
-ls -al /images/origin
 
 
 
+cd "$ORIGIN_DIR"/
+
+squoosh-cli --webp auto ./ -d ../compressed
 
 
- 
-
-squoosh-cli --webp  "$ORIGIN_DIR"/  --output-dir "$COMPRESSED_DIR"
- 
-ls -al  "$COMPRESSED_DIR"
-
-echo "Starting image compression process..."
-# 处理图片
-for file in "$ORIGIN_DIR"/*.{jpg,JPG,jpeg,JPEG,png,PNG}; do
-  if [ -f "$file" ]; then
-    echo "Compressing $file..."
-    squoosh-cli --webp "$file" --output-dir "$COMPRESSED_DIR" &
-    if (( $(jobs -r | wc -l) >= 4 )); then
-      echo "Waiting for a job to finish..."
-      wait -n
-    fi
-  else
-    echo "No file found for $file"
-  fi
-done
-
-echo "Waiting for all jobs to complete..."
-wait
-echo "Image compression process completed."
+ls ../compressed
